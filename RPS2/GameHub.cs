d71fe8d508d2c8f9b86426258c7f453c6352b802
rystills <rystills@gmail.com>
2024-09-25 09:50:21 -0400

    replace locally generated GUIDs with SignalR connectionId

--- RPS2/GameHub.cs
+++ RPS2/GameHub.cs
@@ -2,6 +2,20 @@
 
 public class GameHub : Hub
 {
+    private static HashSet<string> _connectedUsers = new HashSet<string>();
+
+    public override async Task OnConnectedAsync()
+    {
+        _connectedUsers.Add(Context.ConnectionId);
+        await base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(Exception exception)
+    {
+        _connectedUsers.Remove(Context.ConnectionId);
+        await base.OnDisconnectedAsync(exception);
+    }
+    
     public async Task SendMove(string roomCode, string playerMove)
     {
         // broadcast the move to all players in the room
